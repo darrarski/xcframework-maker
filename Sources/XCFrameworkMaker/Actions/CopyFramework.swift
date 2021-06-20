a2509409734b0c7e53f3d729f7ae531d88e29749
Dariusz Rybicki <darrarski@gmail.com>
2021-06-20 18:56:10 +0200

    Add CopyFramework action

---
+++ Sources/XCFrameworkMaker/Actions/CopyFramework.swift
@@ -0,0 +1,35 @@
+/// Creates copy of a framework with provided architectures
+public struct CopyFramework {
+  var run: (Path, [Arch], Path) throws -> Path
+
+  /// Create copy of a framework with provided architectures
+  /// - Parameters:
+  ///   - input: Path to the framework
+  ///   - archs: Architectures to be included in the copied framework
+  ///   - path: Path to a directory where framework copy will be created
+  /// - Throws: Error
+  /// - Returns: Path to the copied framework
+  public func callAsFunction(_ input: Path, archs: [Arch], path: Path) throws -> Path {
+    try run(input, archs, path)
+  }
+}
+
+public extension CopyFramework {
+  static func live(
+    makeDir: MakeDir = .live(),
+    makeCopy: MakeCopy = .live(),
+    removePath: RemovePath = .live(),
+    lipoExtract: LipoExtract = .live()
+  ) -> Self {
+    .init { input, archs, path in
+      try makeDir(path)
+      let output = path.addingComponent(input.lastComponent)
+      try makeCopy(of: input, at: output)
+      let outputBinary = output.addingComponent(output.filenameExcludingExtension)
+      try removePath(outputBinary)
+      let inputBinary = input.addingComponent(input.filenameExcludingExtension)
+      try lipoExtract.callAsFunction(input: inputBinary, archs: archs, output: outputBinary)
+      return output
+    }
+  }
+}
---
+++ Tests/XCFrameworkMakerTests/Actions/CopyFrameworkTests.swift
@@ -0,0 +1,42 @@
+import XCTest
+@testable import XCFrameworkMaker
+
+final class CopyFrameworkTests: XCTestCase {
+  enum Action: Equatable {
+    case didMakeDir(Path)
+    case didMakeCopy(Path, Path)
+    case didRemovePath(Path)
+    case didLipoExtract(Path, [Arch], Path)
+  }
+
+  func testHappyPath() throws {
+    var performedActions = [Action]()
+    let sut = CopyFramework.live(
+      makeDir: .init { path in
+        performedActions.append(.didMakeDir(path))
+      },
+      makeCopy: .init { source, destination in
+        performedActions.append(.didMakeCopy(source, destination))
+      },
+      removePath: .init { path in
+        performedActions.append(.didRemovePath(path))
+      },
+      lipoExtract: .init { input, archs, output in
+        performedActions.append(.didLipoExtract(input, archs, output))
+      }
+    )
+    let input = Path("input/Framework.framework")
+    let archs = [Arch.i386, .arm64]
+    let path = Path("output/path")
+
+    let output = try sut(input, archs: archs, path: path)
+
+    XCTAssertEqual(performedActions, [
+      .didMakeDir(Path("output/path")),
+      .didMakeCopy(Path("input/Framework.framework"), Path("output/path/Framework.framework")),
+      .didRemovePath(Path("output/path/Framework.framework/Framework")),
+      .didLipoExtract(Path("input/Framework.framework/Framework"), [.i386, .arm64], Path("output/path/Framework.framework/Framework"))
+    ])
+    XCTAssertEqual(output, path.addingComponent(input.lastComponent))
+  }
+}
